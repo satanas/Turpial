6f11c9c83fabd9f728de69608dbed55885e384fc
Wil Alvarez <wil.alejandro@gmail.com>
2010-01-07 12:35:58 +0000

    Commit desde mi hogar

--- ui/gtk_ui_main.py
+++ ui/gtk_ui_main.py
@@ -296,10 +296,11 @@ class Main(BaseGui, gtk.Window):
         log.debug(u'Actualizando el timeline')
         gtk.gdk.threads_enter()
         count = self.home.timeline.update_tweets(tweets)
-        username = tweets[0]['user']['screen_name']
-        avatar = tweets[0]['user']['profile_image_url']
-        icon = self.get_user_avatar(username, avatar)
-        self.notify.new_tweets(count, self.updating['home'], tweets[0]['text'], icon)
+        if count > 0:
+            username = tweets[0]['user']['screen_name']
+            avatar = tweets[0]['user']['profile_image_url']
+            icon = self.get_user_avatar(username, avatar)
+            self.notify.new_tweets(count, self.updating['home'], tweets[0]['text'], icon)
         gtk.gdk.threads_leave()
         self.updating['home'] = False
         
@@ -307,10 +308,11 @@ class Main(BaseGui, gtk.Window):
         log.debug(u'Actualizando las replies')
         gtk.gdk.threads_enter()
         count = self.home.replies.update_tweets(tweets)
-        username = tweets[0]['user']['screen_name']
-        avatar = tweets[0]['user']['profile_image_url']
-        icon = self.get_user_avatar(username, avatar)
-        self.notify.new_replies(count, self.updating['replies'], tweets[0]['text'], icon)
+        if count > 0:
+            username = tweets[0]['user']['screen_name']
+            avatar = tweets[0]['user']['profile_image_url']
+            icon = self.get_user_avatar(username, avatar)
+            self.notify.new_replies(count, self.updating['replies'], tweets[0]['text'], icon)
         gtk.gdk.threads_leave()
         self.updating['replies'] = False
         
@@ -318,10 +320,11 @@ class Main(BaseGui, gtk.Window):
         log.debug(u'Actualizando mensajes directos')
         gtk.gdk.threads_enter()
         count = self.home.direct.update_tweets(recv)
-        username = recv[0]['sender']['screen_name']
-        avatar = recv[0]['sender']['profile_image_url']
-        icon = self.get_user_avatar(username, avatar)
-        self.notify.new_directs(count, self.updating['directs'], recv[0]['text'], icon)
+        if count > 0:
+            username = recv[0]['sender']['screen_name']
+            avatar = recv[0]['sender']['profile_image_url']
+            icon = self.get_user_avatar(username, avatar)
+            self.notify.new_directs(count, self.updating['directs'], recv[0]['text'], icon)
         gtk.gdk.threads_leave()
         self.updating['directs'] = False
         

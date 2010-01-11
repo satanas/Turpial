2f90a9a1b8d108757019c8dac6f7f4e75e5a0fa2
Wil Alvarez <wil.alejandro@gmail.com>
2010-01-11 03:34:16 +0000

    Agregado script para la instalación automática de dependencias

---
+++ install-dep.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+echo "Bienvenido al asistente de configuración de Turpial"
+echo "==================================================="
+echo "Este asistente instalará y configurará todas las dependencias necesarias"
+echo "para que Turpial pueda ejecutarse. Presione ENTER para continuar"
+echo ""
+read X
+echo "Actualizando paquetes..."
+aptitude -y update
+echo "Instalando dependencias..."
+aptitude -y install python-simplejson python-imaging python-notify
+echo "Terminado"
+

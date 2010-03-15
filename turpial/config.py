3489e308c8f4b41f1c47db2937d0479c29b46602
Milton Mazzarri <milmazz@gmail.com>
2010-03-15 03:22:28 -0430

    Correcciones que atienden errores y advertencias indicadas por Pylint.
    * Inclusion de documentacion de modulos (docstrings).
    * Se corrige bug al pasar un elemento dinamico como parametro por defecto al
    constructor de la clase ConfigBase.
    * Correccion en los nombres de variables.
    * Ajuste del largo de las lineas de codigo (maximo: 80 caracteres).

--- turpial/config.py
+++ turpial/config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Módulo para manejar los archivos de configuración de usuario del Turpial
+"""Módulo para manejar los archivos de configuración de usuario del Turpial"""
 #
 # Author: Wil Alvarez (aka Satanas)
 # Dic 24, 2009
@@ -56,72 +56,84 @@ DEFAULT_CFG = {
     },
 }
 
+
 class ConfigBase:
-    def __init__(self, default=DEFAULT_CFG):
+    """Configuracion base de la aplicacion"""
+    def __init__(self, default=None):
         self.__config = {}
-        self.default = default
+        if default is None:
+            self.default = DEFAULT_CFG
+        else:
+            self.default = default
         self.cfg = ConfigParser.ConfigParser()
         self.filepath = ''
         self.log = logging.getLogger('Config')
     
     def create(self):
+        """Creacion de fichero de configuracion"""
         self.log.debug('Creando archivo')
-        fd = open(self.filepath,'w')
+        _fd = open(self.filepath,'w')
         for section, v in self.default.iteritems():
             self.cfg.add_section(section)
             for option, value in self.default[section].iteritems():
                 self.cfg.set(section, option, value)
-        self.cfg.write(fd)
-        fd.close()
+        self.cfg.write(_fd)
+        _fd.close()
         
     def load(self):
+        """Carga de fichero de configuracion"""
         self.log.debug('Cargando archivo')
         self.cfg.read(self.filepath)
         
         for section, _v in self.default.iteritems():
-            if not self.__config.has_key(section): self.__config[section] = {}
+            if not self.__config.has_key(section):
+                self.__config[section] = {}
             if not self.cfg.has_section(section): 
                 self.write_section(section, self.default[section])
             for option, value in self.default[section].iteritems():
                 if self.cfg.has_option(section, option):
-                    self.__config[section][option] = self.cfg.get(section, option)
+                    self.__config[section][option] = self.cfg.get(section, 
+                                                                  option)
                 else:
                     self.write(section, option, value)
                 
     def save(self, config):
+        """Guardando configuracion en fichero"""
         self.log.debug('Guardando todo')
-        fd = open(self.filepath,'w')
+        _fd = open(self.filepath,'w')
         for section, _v in config.iteritems():
             for option, value in config[section].iteritems():
                 self.cfg.set(section, option, value)
                 self.__config[section][option] = value
-        self.cfg.write(fd)
-        fd.close()
+        self.cfg.write(_fd)
+        _fd.close()
         
     def write(self, section, option, value):
-        fd = open(self.filepath,'w')
+        _fd = open(self.filepath,'w')
         self.cfg.set(section, option, value)
-        self.cfg.write(fd)
-        fd.close()
+        self.cfg.write(_fd)
+        _fd.close()
         self.__config[section][option] = value
         
     def write_section(self, section, items):
-        fd = open(self.filepath,'w')
+        _fd = open(self.filepath,'w')
         self.cfg.add_section(section)
         for option, value in items.iteritems():
             self.cfg.set(section, option, value)
             self.__config[section][option] = value
         
-        self.cfg.write(fd)
-        fd.close()
+        self.cfg.write(_fd)
+        _fd.close()
         
     def read(self, section, option):
+        """Lectura de opcion de una seccion del fichero de configuracion"""
         try:
             return self.__config[section][option]
         except Exception:
             return None
             
     def read_section(self, section):
+        """Lectura de seccion de fichero de configuracion"""
         self.log.debug(u'Leyendo sección %s' % (section))
         try:
             return self.__config[section]
@@ -129,17 +141,23 @@ class ConfigBase:
             return None
             
     def read_all(self):
+        """Lectura del fichero de configuracion"""
         self.log.debug('Leyendo todo')
         try:
             return self.__config
         except Exception:
             return None
 
+
 class ConfigHandler(ConfigBase):
+    """Manejador de la configuracion, creacion de estructura inicial de
+    directorios, archivos y listas."""
+
     def __init__(self, user):
         ConfigBase.__init__(self)
         
-        self.dir = os.path.join(os.path.expanduser('~'), '.config', 'turpial', user)
+        self.dir = os.path.join(os.path.expanduser('~'), '.config', 
+                                'turpial', user)
         self.imgdir = os.path.join(self.dir, 'images')
         self.filepath = os.path.join(self.dir, 'config')
         self.mutedpath = os.path.join(self.dir, 'muted')
@@ -155,29 +173,30 @@ class ConfigHandler(ConfigBase):
         
         self.load()
         
-        #if self.__config['App']['version'] != DEFAULT['App']['version']:
-        #    self.write('App', 'version', DEFAULT['App']['version'])
-    
     def create_muted_list(self):
-        fd = open(self.mutedpath,'w')
-        fd.close()
+        _fd = open(self.mutedpath,'w')
+        _fd.close()
         
     def load_muted_list(self):
         muted = []
-        fd = open(self.mutedpath,'r')
-        for line in fd:
-            if line == '\n': continue
+        _fd = open(self.mutedpath,'r')
+        for line in _fd:
+            if line == '\n':
+                continue
             muted.append(line.strip('\n'))
-        fd.close()
+        _fd.close()
         return muted
         
     def save_muted_list(self, lst):
-        fd = open(self.mutedpath,'w')
+        _fd = open(self.mutedpath,'w')
         for user in lst:
-            fd.write(user+'\n')
-        fd.close()
+            _fd.write(user+'\n')
+        _fd.close()
+
 
 class ConfigApp(ConfigBase):
+    """Configuracion de la aplicacion"""
+    
     def __init__(self):
         ConfigBase.__init__(self, default=GLOBAL_CFG)
         

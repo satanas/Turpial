7cf026180ecf5e9dd64de24a9813343367f97ebf
Sebastián Ramírez Magrí <sebasmagri@gmail.com>
2011-03-20 05:28:36 -0430

    Fix SyntaxError in latest changes

--- turpial/api/protocols/twitter/twitter.py
+++ turpial/api/protocols/twitter/twitter.py
@@ -55,8 +55,8 @@ class Twitter(Protocol):
         
         in_reply_to_id = None
         in_reply_to_user = None
-        if tweet.has_key('in_reply_to_status_id') and
-           tweet['in_reply_to_status_id']:
+        if (tweet.has_key('in_reply_to_status_id') and
+           tweet['in_reply_to_status_id']):
             in_reply_to_id = tweet['in_reply_to_status_id']
             in_reply_to_user = tweet['in_reply_to_screen_name']
             

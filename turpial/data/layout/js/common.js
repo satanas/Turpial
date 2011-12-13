5b18f1801d0e65bfcb7522e062a99a4c7083f9cf
satanas <wil.alejandro@gmail.com>
2011-12-13 12:25:52 -0300

    Modified notice functions to add support for several widget notifications

--- turpial/data/layout/js/common.js
+++ turpial/data/layout/js/common.js
@@ -6,26 +6,27 @@ function exec_command(cmd) {
     window.location = cmd;
 }
 
-function show_notice(message, type) {
-    $('#notice').html(message);
-    $('#notice').fadeIn();
-    $('#notice').addClass(type);
-    timeout = setTimeout(hide_notice, 5000);
+function show_notice(container, message, type) {
+    var cid = '#' + container + '-notice';
+    $(cid).html(message);
+    $(cid).fadeIn();
+    $(cid).addClass(type);
+    timeout = setTimeout(function(){hide_notice(cid)}, 5000);
 }
 
-function hide_notice(force) {
+function hide_notice(cid, force) {
     clearTimeout(timeout);
     if (force == undefined)
-        $('#notice').fadeOut();
+        $(cid).fadeOut();
     else if (force == true)
-        $('#notice').hide();
-    reset = setTimeout(reset_notice, 500);
+        $(cid).hide();
+    reset = setTimeout(function(){reset_notice(cid)}, 500);
 }
 
-function reset_notice() {
+function reset_notice(cid) {
     clearTimeout(reset);
-    $('#notice').html('');
-    $('#notice').removeClass('error');
-    $('#notice').removeClass('warning');
-    $('#notice').removeClass('info');
+    $(cid).html('');
+    $(cid).removeClass('error');
+    $(cid).removeClass('warning');
+    $(cid).removeClass('info');
 }

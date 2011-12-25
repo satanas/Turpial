df415a3fc457f28846b63b8aa986098448e4d5ea
satanas <wil.alejandro@gmail.com>
2011-12-25 20:26:40 -0300

    Removed hardcoded data for testing profiles

--- turpial/ui/gtk/main.py
+++ turpial/ui/gtk/main.py
@@ -465,7 +465,6 @@ class Main(Base, Singleton, gtk.Window):
                 self.delete_status_response)
     
     def show_profile(self, account_id, username):
-        username = 'satanas'
         self.worker.register(self.core.get_user_profile, (account_id, username),
                 self.show_profile_response)
     

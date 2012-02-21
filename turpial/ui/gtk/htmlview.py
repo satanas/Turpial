f434a76412d06d28522c8661d26a45e851def90d
satanas <wil.alejandro@gmail.com>
2012-02-21 11:51:07 -0300

    Trying some improvements in webkit webview

--- turpial/ui/gtk/htmlview.py
+++ turpial/ui/gtk/htmlview.py
@@ -26,15 +26,18 @@ class HtmlView(gtk.VBox, gobject.GObject):
         self.uri = 'file://' + os.path.dirname(__file__)
         self.settings = webkit.WebSettings()
         self.settings.set_property('enable-default-context-menu', False)
-        '''
+        
         self.settings.enable_java_applet = False
         self.settings.enable_plugins = False
-        self.settings.enable_page_cache = True
+        self.settings.enable_page_cache = False
         self.settings.enable_offline_web_application_cache = False
         self.settings.enable_html5_local_storage = False
         self.settings.enable_html5_database = False
         self.settings.enable_default_context_menu = False
-        '''
+        self.settings.enable_xss_auditor = False
+        self.settings.enable_dns_prefetching = False
+        self.settings.resizable_text_areas = False
+        
         self.view = webkit.WebView()
         self.view.set_settings(self.settings)
         self.view.connect('load-started', self.__started)

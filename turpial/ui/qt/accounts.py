a7152bfbc79cae1a41f1958bc4a3981c6fa572bd
Carlos Guerrero <guerrerocarlos@gmail.com>
2012-03-26 22:58:11 -0600

    [IMP] Showing AccountsDialog in PyQt4

--- turpial/ui/qt/accounts.py
+++ turpial/ui/qt/accounts.py
@@ -1,120 +1,152 @@
 # -*- coding: utf-8 -*-
 
-# PyQT account manager view for Turpial
+# GTK account manager for Turpial
 #
-# Author:  Carlos Guerrero (aka guerrerocarlos)
-# Started: Sep 11, 2011
+# Author: Wil Alvarez (aka Satanas)
+# Nov 13, 2011
 
 from PyQt4 import QtGui
 from PyQt4.QtWebKit import *
+import gtk
 import logging
 
+from turpial.ui.lang import i18n
 from turpial.ui.html import HtmlParser
-from turpial.ui.qt.worker import Worker
 from turpial.ui.qt.htmlview import HtmlView
-#from turpial.ui.gtk.oauthwin import OAuthWindow
-from turpial.ui.qt.account_form import AccountForm
 
-log = logging.getLogger('PyQt4')
+log = logging.getLogger('Qt-AccountsDialog')
 
 class AccountsDialog(QtGui.QDialog):
     def __init__(self, parent):
         super(AccountsDialog,self).__init__()
+        #gtk.Window.__init__(self)
         
-        # Main tools
         self.mainwin = parent
-        self.core = parent.core
-        self.worker = None  
-        
-        self.htmlparser = HtmlParser(None)
+        self.htmlparser = HtmlParser()
+        #self.set_title(i18n.get('accounts'))
         self.setWindowTitle("Account Manager")
-        self.resize(410,350)
-        self.finished.connect(self.__close)
+        self.resize(360,320)
+        #self.set_size_request(360, 320)
+        #self.set_resizable(False)
+        #self.set_icon(self.mainwin.load_image('turpial.png', True))
+        #self.set_transient_for(parent)
+        #self.set_position(gtk.WIN_POS_CENTER_ON_PARENT)
+        #self.set_gravity(gtk.gdk.GRAVITY_STATIC)
+        #self.connect('delete-event', self.__close)
         
-        self.container = HtmlView(self)
-        self.container.view.page().setLinkDelegationPolicy(QWebPage.DelegateAllLinks)
-        self.container.view.page().linkClicked.connect(self.__action_request)
+        self.container = HtmlView()
+        #self.container.connect('action-request', self.__action_request)
+        self.container.action_request.connect(self.__action_request)
+
         self.layout = QtGui.QVBoxLayout()
         self.layout.setContentsMargins(0,0,0,0)
         self.layout.addWidget(self.container.view)
         self.setLayout(self.layout)
 
         self.showed = False
-        self.form = None
-        self.acc_id = None
-        
-    def show_all(self, accounts):
-        self.showed = True
-        if not self.worker:
-            self.worker = Worker()
-            self.worker.set_timeout_callback(self.__timeout_callback)
-            self.worker.start()
-        page = self.htmlparser.accounts(accounts)
-        self.container.setHtml(page)
-        self.show()
+        self.showed = True 
+        #self.form = None
+#        self.show_now()
     
-    def __close(self, event=None):
+    def __close(self, widget, event=None):
         self.showed = False
         self.hide()
         return True
     
-    def __action_request(self, url):
-        url = url.toString()
-        action = url.split(':')[1]
-        try:
-            args = url.split(':')[1].split('-%&%-')
-        except IndexError:
-            args = []
+    def __action_request(self, widget, url):
+        action, args = self.htmlparser.parse_command(url)
         
-        if action == "//close":
-            self.__close()
-        elif action == "//new_account":
-            self.form = AccountForm(self, self.core.list_protocols())
-        elif action == "//delete_account":
-            self.delete_account(args[0])
-        
-    def __login_callback(self, arg):
-        print 'resp', arg, arg.code, arg.errmsg, arg.items
-        
-        if arg.code > 0:
-            #msg = i18n.get(rtn.errmsg)
-            msg = arg.errmsg
-            self.form.cancel_login(msg)
-            return
-        
-        auth_obj = arg.items
-        if auth_obj.must_auth():
-            oauthwin = OAuthWindow(self)
-            oauthwin.connect('response', self.__oauth_callback)
-            oauthwin.connect('cancel', self.__cancel_callback)
-            oauthwin.open(auth_obj.url)
+        if action == "close":
+            self.__close(widget)
+        elif action == "new_account":
+            self.form = AccountForm(self.mainwin, self)
+        elif action == "delete_account":
+            self.mainwin.delete_account(args[0])
+        elif action == "login":
+            self.mainwin.single_login(args[0])
     
-    def __cancel_callback(self):
-        pass
-        
-    def __oauth_callback(self, verifier):
-        self.worker.register(self.core.authorize_oauth_token, (self.acc_id, verifier), self.__auth_callback)  
+    def update(self):
+        if self.showed:
+            page = self.htmlparser.accounts(self.mainwin.get_all_accounts())
+            self.container.render(page)
     
-    def __auth_callback(self):
-        self.worker.register(self.core.auth, (self.acc_id), self.__done_callback)
-
-    def __timeout_callback(self, funct, arg):
-        pass
-        
-    def __done_callback(self, arg):
-        if arg.code > 0:
-            msg = arg.errmsg
-            self.form.cancel_login(msg)
+    def cancel_login(self, message):
+        if self.form:
+            self.form.cancel(message)
+            return True
+        self.update()
+        return False
+    
+    def done_login(self):
+        if self.form:
+            self.form.done()
+            return True
+        return False
+    
+    def show_now(self):
+        if self.showed:
+            print "showed"
+            pass
+            #self.present()
         else:
-            self.form.done_login()
-            page = self.htmlparser.render_account_list(self.core.all_accounts())
-            self.container.update_element("list", page)
+            print "showing"
+            self.showed = True
+            self.update()
+            #self.show_all()
+            self.show()
+    
+    def quit(self):
+        self.destroy()
+
+class AccountForm(gtk.Window):
+    def __init__(self, mainwin, parent, user=None, pwd=None, protocol=None):
+        gtk.Window.__init__(self)
         
-    def delete_account(self, account_id):
-        self.core.unregister_account(account_id, True)
-        page = self.htmlparser.render_account_list(self.core.all_accounts())
-        self.container.update_element("list", page)
+        self.mainwin = mainwin
+        self.set_transient_for(parent)
+        self.set_modal(True)
+        self.htmlparser = HtmlParser()
+        self.set_title(i18n.get('create_account'))
+        self.set_size_request(290, 200)
+        self.set_resizable(False)
+        self.set_position(gtk.WIN_POS_CENTER)
+        self.set_gravity(gtk.gdk.GRAVITY_STATIC)
+        self.connect('delete-event', self.__close)
         
-    def save_account(self, username, protocol_id, password):
-        self.acc_id = self.core.register_account(username, protocol_id, password, True)
-        self.worker.register(self.core.login, (self.acc_id), self.__login_callback)
+        self.container = HtmlView()
+        self.container.connect('action-request', self.__action_request)
+        self.add(self.container)
+        
+        page = self.htmlparser.account_form(self.mainwin.get_protocols_list())
+        self.container.render(page)
+        self.show_all()
+        self.working = False
+        
+    def __close(self, widget, event=None):
+        if not self.working:
+            self.destroy()
+            return False
+        else:
+            return True
+    
+    def __action_request(self, widget, url):
+        action, args = self.htmlparser.parse_command(url)
+        
+        if action == "close":
+            self.__close(widget)
+        elif action == "save_account":
+            self.working = True
+            self.mainwin.save_account(args[0], args[1], args[2])
+    
+    def cancel(self, message):
+        self.working = False
+        self.container.execute("cancel_login('" + message + "');")
+    
+    def set_loading_message(self, message):
+        pass
+        #self.container.execute("set_loading_message('" + message + "');")
+        
+    def done(self):
+        self.working = False
+        self.destroy()
+

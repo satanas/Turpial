81a074052614860d75ba8fb46ca6e70ee867b95f
satanas <wil.alejandro@gmail.com>
2012-05-12 18:05:38 -0300

    Fixed overflow in empty.template layout

--- turpial/data/layout/js/empty.js
+++ turpial/data/layout/js/empty.js
@@ -12,15 +12,15 @@ function recalculate_column_size(nw, nh) {
         width = nw;
     if (nh != undefined)
         height = nh;
-    
-    var content_height = height - 23;
-    var column_width = width - 1;
+
+    var content_height = height - 25;
+    var column_width = width - 2;
     var column_height = content_height;
     var wrapper_height = column_height;
     var list_width = column_width - 11;
     var list_height = column_height - 13;
     var empty_logo_top = list_height / 5;
-    
+
     $('#content').css('height', content_height + 'px');
     $('.column').css('width', column_width + 'px');
     $('.column').css('height', column_height + 'px');

0a67200144d0d08b670b58a8918d265b6ebcf44b
satanas <wil.alejandro@gmail.com>
2012-05-26 20:35:48 -0300

    Improved error message when Turpial tries to import Indicate module

--- turpial/main.py
+++ turpial/main.py
@@ -5,33 +5,22 @@
 #
 # Author: Wil Alvarez (aka Satanas)
 # Oct 7, 2011
-import time #quitar, solo aprendiendo pyinstaller
 
+import os
 import sys
 import logging
 
 from optparse import OptionParser, SUPPRESS_HELP
 
-class OptParser(OptionParser):
-    def __init__(self):
-        OptionParser.__init__(self)
-    def error(self, error):
-        pass
-
-
 from turpial import VERSION
 from turpial.ui import util
-from libturpial import VERSION as LIBVERSION
-from libturpial.common.tools import *
 from libturpial.api.core import Core
+from libturpial.common.tools import *
 from libturpial.config import AppConfig
+from libturpial import VERSION as LIBVERSION
 
-#from PyQt4.Qt import *
-#from PyQt4.QtGui import *
-#from PyQt4.QtCore import *
+LOG_FMT = logging.Formatter('[%(asctime)s] [%(name)s::%(levelname)s] %(message)s', '%Y%m%d-%H:%M')
 
-import sys
-import os
 
 class Turpial:
     def __init__(self):
@@ -65,6 +54,9 @@ class Turpial:
         else:
             logging.basicConfig(level=logging.INFO)
         self.log = logging.getLogger('Controller')
+        #handler = logging.StreamHandler()
+        #handler.setFormatter(LOG_FMT)
+        #self.log.addHandler(handler)
 
         self.core = Core()
 
--- turpial/notification.py
+++ turpial/notification.py
@@ -25,15 +25,15 @@ class Notification:
     def __init__(self, disable=False):
         self.activate()
         self.disable = disable
-        
+
         if not NOTIFY:
             log.debug('Module not available')
             self.disable = True
             return
-        
+
         if disable:
             log.debug('Module disabled')
-        
+
     def toggle_activation(self):
         if self.active:
             self.active = False
@@ -42,20 +42,20 @@ class Notification:
 
     def activate(self):
         self.active = True
-        
+
     def deactivate(self):
         self.active = False
-        
+
     def popup(self, title, message, icon=None):
         if self.disable:
             log.debug('Module disabled. Showing no notifications')
             return
-        
+
         global NOTIFY
         if self.active and NOTIFY:
             if pynotify.init("Turpial"):
                 if not icon:
-                    iconpath = os.path.join(os.path.dirname(__file__), 'data', 
+                    iconpath = os.path.join(os.path.dirname(__file__), 'data',
                         'pixmaps', 'turpial-notification.png')
                     icon = os.path.realpath(iconpath)
                 icon = "file://%s" % icon
@@ -65,7 +65,7 @@ class Notification:
                 except GError:
                     log.debug('Notification service not running')
                     NOTIFY = False
-    
+
     def updates(self, column, count):
         object_name = ''
         if column.protocol_id == 'twitter':
@@ -78,11 +78,11 @@ class Notification:
                 object_name = i18n.get('new_dents')
             else:
                 object_name = i18n.get('new_dent')
-        message = "%s :: %s (%s)" % (column.account_id.split('-')[0], 
+        message = "%s :: %s (%s)" % (column.account_id.split('-')[0],
             column.column_name, i18n.get(column.protocol_id))
-        
+
         self.popup('%i %s' % (count, object_name), message)
-        
+
     def login(self, profile):
         object_name = ''
         if profile.get_protocol_id() == 'twitter':
@@ -95,20 +95,20 @@ class Notification:
                 object_name = i18n.get('dents')
             else:
                 object_name = i18n.get('dent')
-        
+
         self.popup('@%s' % profile.username,
-            '%s: %i\n%s: %i\n%s: %i' % 
+            '%s: %i\n%s: %i\n%s: %i' %
             (object_name, profile.statuses_count,
-            i18n.get('following'), profile.friends_count, 
+            i18n.get('following'), profile.friends_count,
             i18n.get('followers'), profile.followers_count))
-    
+
     def following(self, user, follow):
         name = user.username
         if follow:
             self.popup(i18n.get('turpial_follow'), i18n.get('you_are_now_following') % name)
         else:
             self.popup(i18n.get('turpial_unfollow'), i18n.get('you_are_no_longer_following') % name)
-                
+
     def following_error(self, message, follow):
         if follow:
             self.popup(i18n.get('turpial_follow'), message)

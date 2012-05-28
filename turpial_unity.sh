53491b83790b82a602f729e056ee4d101ea1755e
4ndreaSt4gi <stagi.andrea@gmail.com>
2012-05-29 01:27:32 +0200

    Old script for Unity removed

--- turpial_unity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Author: Andrea Stagi (4ndreaSt4gi)
-# Description: launches Turpial in Ubuntu with Unity support
-# License: GPL v3
-
-python ./turpial/ui/unity/daemon.py start
-turpial
-python ./turpial/ui/unity/daemon.py stop

498c2e8ccfd8655d68ea536b2cadd4b47535f2f9
satanas <wil.alejandro@gmail.com>
2012-12-25 10:20:55 -0300

    Added functionality to show searches when user clicks over a hashtag

--- turpial/ui/gtk/statuswidget.py
+++ turpial/ui/gtk/statuswidget.py
@@ -145,7 +145,8 @@ class StatusWidget(Gtk.EventBox):
 
     def __highlight_hashtags(self, status, text):
         for h in status.entities['hashtags']:
-            cad = '<a href="hashtags:%s">%s</a>' % (h.url, h.display_text)
+            url = "%s-search:%%23%s" % (self.status.account_id, h.display_text[1:])
+            cad = '<a href="hashtags:%s">%s</a>' % (url, h.display_text)
             text = text.replace(h.search_for, cad)
         return text
 
@@ -167,7 +168,7 @@ class StatusWidget(Gtk.EventBox):
         if url.startswith('http'):
             self.base.open_url(url)
         elif url.startswith('hashtag'):
-            print "Opening hashtag"
+            self.base.save_column(url[9:])
         elif url.startswith('groups'):
             print "Opening groups"
         elif url.startswith('profile'):

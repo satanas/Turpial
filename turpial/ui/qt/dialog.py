a6e4d9c227c4259cea584bfdc608838e4f402570
satanas <wil.alejandro@gmail.com>
2013-07-14 13:58:59 -0400

    Added modal dialog class

---
+++ turpial/ui/qt/dialog.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Qt modal dialog for Turpial
+
+from PyQt4.QtCore import Qt
+from PyQt4.QtGui import QDialog
+
+class ModalDialog(QDialog):
+    def __init__(self, width, height):
+        QDialog.__init__(self)
+        self.setFixedSize(width, height)
+        self.setWindowFlags(Qt.Window | Qt.WindowTitleHint | Qt.WindowCloseButtonHint | Qt.CustomizeWindowHint)
+        self.setModal(True)

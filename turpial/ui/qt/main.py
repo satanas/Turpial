3614e4ed66d74a6c7fde258d7cdb46be9e1139e9
Sebastián Magrí <sebasmagri@gmail.com>
2013-11-10 09:26:20 -0430

    Set icon and application name in the QT UI

--- turpial/ui/qt/main.py
+++ turpial/ui/qt/main.py
@@ -5,26 +5,17 @@
 import os
 import sys
 import urllib2
-import traceback
 
 from functools import partial
 
-from PyQt4.QtGui import QMenu
-from PyQt4.QtGui import QImage
-from PyQt4.QtGui import QWidget
-from PyQt4.QtGui import QAction
-from PyQt4.QtGui import QPixmap
-from PyQt4.QtGui import QDialog
-from PyQt4.QtGui import QMessageBox
-from PyQt4.QtGui import QVBoxLayout
-from PyQt4.QtGui import QApplication
-from PyQt4.QtGui import QFontDatabase
+from PyQt4.QtGui import (
+    QMenu, QImage, QWidget, QAction, QPixmap, QDialog, QMessageBox,
+    QVBoxLayout, QApplication, QFontDatabase, QIcon
+)
 
-from PyQt4.QtCore import QTimer
-from PyQt4.QtCore import pyqtSignal
+from PyQt4.QtCore import QTimer, pyqtSignal
 
-from turpial import DESC
-from turpial.ui.base import *
+from turpial.ui.base import *  # NOQA
 
 from turpial.ui.qt.dock import Dock
 from turpial.ui.qt.tray import TrayIcon
@@ -52,7 +43,7 @@ class Main(Base, QWidget):
     account_registered = pyqtSignal()
 
     def __init__(self):
-        self.app = QApplication(sys.argv)
+        self.app = QApplication(['Turpial'] + sys.argv)
 
         Base.__init__(self)
         QWidget.__init__(self)
@@ -62,14 +53,15 @@ class Main(Base, QWidget):
         QFontDatabase.addApplicationFont(os.path.join(self.fonts_path, 'TitilliumWeb-Regular.ttf'))
         QFontDatabase.addApplicationFont(os.path.join(self.fonts_path, 'Monda-Regular.ttf'))
 
-        database = QFontDatabase()
-        #for f in database.families():
-        #    print f
+        # database = QFontDatabase()
+        # for f in database.families():
+        #     print f
 
         self.templates_path = os.path.realpath(os.path.join(
             os.path.dirname(__file__), 'templates'))
 
         self.setWindowTitle('Turpial')
+        self.setWindowIcon(QIcon(self.get_image_path('turpial.svg')))
         self.ignore_quit = True
         self.resize(320, 480)
         self.showed = True
